3601b135d203a602fe82cdc7f6606f26a276384e
Andrei Kucherenko <a.kucherenko@okko.tv>
2024-09-15 12:36:54 +0300

    Initial Commit

---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by Andrey Kucherenko on 15.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
